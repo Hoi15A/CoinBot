eafde38b4c9ea63eb49b3862dd840327d07172fe
Austin Albrecht <austin.aa@me.com>
2017-05-15 22:36:41 +0200

    Added ETH

--- btc.js
+++ btc.js
@@ -4,6 +4,7 @@ var http = require('http');
 
 function getCurrentBTC(callback) {
   //https://blockchain.info/en/ticker
+
   return http.get({
         host: 'blockchain.info',
         path: '/en/ticker'
@@ -18,11 +19,40 @@ function getCurrentBTC(callback) {
 
             // Data reception is done, do whatever with it!
             var parsed = JSON.parse(body);
-            console.log(parsed.CHF);
+            callback(parsed.CHF);
         });
 
     });
 
 }
 
-getCurrentBTC();
+function getCurrentETH(callback) {
+  //https://coinmarketcap-nexuist.rhcloud.com/api/eth
+  
+  return http.get({
+        host: 'coinmarketcap-nexuist.rhcloud.com',
+        path: '/api/eth'
+    }, function(response) {
+
+        // Continuously update stream with data
+        var body = '';
+        response.on('data', function(d) {
+            body += d;
+        });
+        response.on('end', function() {
+
+            // Data reception is done, do whatever with it!
+            var parsed = JSON.parse(body);
+            callback(parsed.price.chf);
+        });
+
+    });
+
+}
+
+getCurrentBTC(function(chf) {
+  console.log(chf);
+});
+getCurrentETH(function(eth) {
+  console.log(eth);
+});
