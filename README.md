b796249d1fb4fc47271624283cd58e39fb167064
Austin Albrecht <austin.aa@me.com>
2017-11-10 21:37:37 +0100

    Switched to new api, made bot more customisable and made general improvements

--- README.md
+++ README.md
@@ -6,5 +6,5 @@ A small node script that will send a 1 time message to telegram
 - Clone the repo `git clone https://github.com/Hoi15A/CoinBot.git`
 - Run `npm install`
 - Copy `example.telegram.json` to `telegram.json`
-- In `telegram.json` change `token` to your telegram bot token and `chat` to a chat or channel id
+- In `telegram.json` change `token` to your telegram bot token and `chat` to a chat or channel id. Under `crypto` you can specify an array of cryptocurrencies to be sent by the bot and with `currency` you can set the currency it should convert the value of the crypto to. (both need to be available on the [coinmarketcap.com api](https://api.coinmarketcap.com/v1/ticker/))
 - Run `node btc.js` in a cronjob at whatever interval you like
--- btc.js
+++ btc.js
@@ -1,81 +1,63 @@
-var http = require('http');
-var fs = require('fs');
-var request = require('request');
+var fs = require('fs')
+var path = require('path')
+var request = require('request')
 
-var tg = JSON.parse(fs.readFileSync(__dirname + '/telegram.json', 'utf8'));
+var tg = JSON.parse(fs.readFileSync(path.join(__dirname, '/telegram.json'), 'utf8'))
 
-console.log("Bot token: " + tg.token);
-console.log("Chat/Channel: " + tg.chat + "\n");
+console.log('Bot token: ' + tg.token)
+console.log('Chat/Channel: ' + tg.chat)
+console.log('Cryptocurrencies: ' + tg.crypto)
+console.log('Currency: ' + tg.currency + '\n')
 
-function getCurrentBTC(callback) {
+function getCoinValues (callback) {
+  const options = {
+    url: 'https://api.coinmarketcap.com/v1/ticker/',
+    method: 'GET',
+    headers: {
+      'Accept': 'application/json',
+      'Accept-Charset': 'utf-8'
+    },
+    qs: {
+      convert: tg.currency
+    }
+  }
 
-  return http.get({
-        host: 'blockchain.info',
-        path: '/en/ticker'
-    }, function(response) {
-
-        // Continuously update stream with data
-        var body = '';
-        response.on('data', function(d) {
-            body += d;
-        });
-        response.on('end', function() {
-
-            // Data reception is done, do whatever with it!
-            var parsed = JSON.parse(body);
-            callback(parsed.CHF.buy.toFixed(2));
-        });
-
-    });
-
+  request(options, function (err, res, body) {
+    if (err) {
+      console.error(err)
+      sendToTelegram('An error occured, please check the log.')
+      return
+    }
+    let data = JSON.parse(body)
+    callback(data)
+  })
 }
 
-function getCurrentETH(callback) {
-
-  return http.get({
-        host: 'coinmarketcap-nexuist.rhcloud.com',
-        path: '/api/eth'
-    }, function(response) {
-
-        // Continuously update stream with data
-        var body = '';
-        response.on('data', function(d) {
-            body += d;
-        });
-        response.on('end', function() {
-
-            // Data reception is done, do whatever with it!
-            var parsed = JSON.parse(body);
-            callback(parsed.price.chf.toFixed(2));
-        });
-
-    });
-
-}
-
-function sendToTelegram(message) {
-  var url = "https://api.telegram.org/bot" + tg.token + "/sendMessage"
+function sendToTelegram (message) {
+  var url = 'https://api.telegram.org/bot' + tg.token + '/sendMessage'
   request.post(
     url,
     { json: {
       chat_id: tg.chat,
       text: message,
-      parse_mode: "Markdown",
+      parse_mode: 'Markdown',
       disable_web_page_preview: true
     } },
     function (error, response, body) {
-        if (!error && response.statusCode == 200) {
-            console.log(body)
-        }
-    }
-);
-
+      if (!error && response.statusCode === 200) {
+        console.log(body)
+      }
+    })
 }
 
-getCurrentBTC(function(chf) {
-  var msg = "`1` *Bitcoin* is worth `" + chf + "` *CHF*\n";
-  getCurrentETH(function(chf) {
-    msg += "`1` *Ethereum* is worth `" + chf + "` *CHF* ";
-    sendToTelegram(msg);
-  });
-});
+getCoinValues(function (data) {
+  var message = []
+  for (var i = 0; i < data.length; i++) {
+    for (var j = 0; j < tg.crypto.length; j++) {
+      if (data[i].symbol === tg.crypto[j]) {
+        message.push('1 *' + data[i].name + '* is worth ' + parseFloat(data[i]['price_' + tg.currency.toLowerCase()]).toFixed(2) + ' *' + tg.currency + '*')
+      }
+    }
+  }
+  sendToTelegram(message.join('\n'))
+})
